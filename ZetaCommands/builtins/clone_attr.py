88973d73d7fd8c54996bdceef5ed2a6dfe903f08
ZetaDaemon <53562820+ZetaDaemon@users.noreply.github.com>
2022-06-03 12:04:03 +1000

    Add files via upload

---
+++ ZetaCommands/builtins/clone_attr.py
@@ -0,0 +1,71 @@
+import unrealsdk
+import argparse
+import functools
+from typing import Callable, Dict
+
+from Mods.CommandExtensions import RegisterConsoleCommand
+from Mods.CommandExtensions.builtins import is_obj_instance, obj_name_splitter, parse_object
+from Mods.CommandExtensions.builtins.clone import clone_object, parse_clone_target
+
+contextresolverchain = "ContextResolverChain"
+valueresolverchain = "ValueResolverChain"
+
+def object_to_clone(base: unrealsdk.UObject, flag: bool):
+	if flag:
+		return base.ObjectArchetype
+	else:
+		return base
+
+def handle_ResolverChain(base: unrealsdk.UObject, clone: unrealsdk.UObject, ResolverChainName: str, flag: bool):
+	ResolverChain = getattr(base, ResolverChainName)
+	cloned_ResolverChain = []
+	try:
+		for Resolver in ResolverChain:
+			if Resolver.PathName(Resolver) == "WillowGame.Default__DesignerAttributeDefinition:ValueResolver":
+				cloned_ResolverChain.append(Resolver)
+			else:
+				clonedResolver = clone_object(object_to_clone(Resolver, flag), clone, "")
+				cloned_ResolverChain.append(clonedResolver)
+	except:
+		unrealsdk.Log(f"clone_attr: Error on line 22")
+	setattr(clone, ResolverChainName, cloned_ResolverChain) 
+
+
+def handler(args: argparse.Namespace) -> None:
+	src = parse_object(args.base)
+	if src is None:
+		return
+	if not is_obj_instance(src, "AttributeDefinition"):
+		unrealsdk.Log(f"Object {src.PathName(src)} must be a 'AttributeDefinition'!")
+		return
+
+	outer, name = parse_clone_target(args.clone, src.Class.Name, args.suppress_exists)
+	if name is None:
+		return
+	cloned = clone_object(object_to_clone(src, args.use_default), outer, name)
+
+	handle_ResolverChain(src, cloned, contextresolverchain, args.use_default)
+	handle_ResolverChain(src, cloned, valueresolverchain, args.use_default)
+
+
+
+parser = RegisterConsoleCommand(
+	"clone_attr",
+	handler,
+	splitter=obj_name_splitter,
+	description=(
+		"Clones an attribute as well as the resolver chains."
+	)
+)
+parser.add_argument("base", help="The WeaponBalanceDefinition to create a copy of.")
+parser.add_argument("clone", help="The name of the clone to create.")
+parser.add_argument(
+	"-x", "--suppress-exists",
+	action="store_true",
+	help="Suppress the error message when an object already exists."
+)
+parser.add_argument(
+	"-d", "--use-default",
+	action="store_true",
+	help="Uses the ObjectArchetype of the base attribute to create the clone rather than the base attribute itself"
+)
