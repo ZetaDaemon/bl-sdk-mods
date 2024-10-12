8478cbbd41c0f5f96f3065b436ff38698404c1ad
mopioid <smr.accounts+mopioid@icloud.com>
2024-10-12 11:13:38 -0700

    DialogSkipper 1.2 with Always Skip keybind

--- DialogSkipper/README.md
+++ DialogSkipper/README.md
@@ -1,7 +1,10 @@
 # Dialog Skipper
 Allows dialog to be skipped by pressing a keybind, defaults to Z.
 
-Additionally, allows for the option to automatically skip all dialog so it never plays.
+Additionally, allows for the option to automatically skip all dialog so it never plays. This is also toggleable via a keybind, defaults to X.
+
+### v1.2
+Added a keybind to toggle dialog always being skipped.
 
 ### v1.1
 Fixed some dialog not being skipped with always skip dialog on.
--- DialogSkipper/__init__.py
+++ DialogSkipper/__init__.py
@@ -1,5 +1,5 @@
 import unrealsdk
-from Mods.ModMenu import ModTypes, RegisterMod, SDKMod, EnabledSaveType, Keybind, Options, Mods, Hook
+from Mods.ModMenu import ModTypes, RegisterMod, SDKMod, EnabledSaveType, Keybind, Options, Mods, Hook, SaveModSettings
 
 class DialogSkipper(SDKMod):
 	Name: str = "Dialog Skipper"
@@ -7,12 +7,13 @@ class DialogSkipper(SDKMod):
 	Description: str = (
 		"Allows for skipping dialog."
 	)
-	Version: str = "1.1"
+	Version: str = "1.2"
 	Types: ModTypes = ModTypes.Utility
 	SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadWithSettings
 
 	Keybinds: list = [
-		Keybind("Skip Dialog", "Z")
+		Keybind("Skip Dialog", "Z"),
+		Keybind("Toggle Always Skip", "X")
 	]
 
 	def __init__(self):
@@ -28,12 +29,44 @@ class DialogSkipper(SDKMod):
 			self.always_skip
 		]
 	
+	def training_text(self, message: str, duration: float = 2.0) -> None:
+		pc = unrealsdk.GetEngine().GamePlayers[0].Actor
+
+		hud_movie = pc.GetHUDMovie()
+		if hud_movie is None:
+			return
+
+		hud_movie.ClearTrainingText()
+		hud_movie.AddTrainingText(
+			message,
+			self.Name,
+			duration,
+			(),
+			"",
+			False,
+			0,
+			pc.PlayerReplicationInfo,
+			True
+		)
+
+	def skip_dialog(self) -> None:
+		for dialog in unrealsdk.FindAll("GearboxDialogComponent"):
+			if not dialog.IsTalking():
+				continue
+			dialog.StopTalking()
+
 	def GameInputPressed(self, input):
 		if input.Name == "Skip Dialog":
-			for dialog in unrealsdk.FindAll("GearboxDialogComponent"):
-				if not dialog.IsTalking():
-					continue
-				dialog.StopTalking()
+			self.skip_dialog()
+		elif input.Name == "Toggle Always Skip":
+			if self.always_skip.CurrentValue:
+				self.always_skip.CurrentValue = False
+				self.training_text("Always Skip: Off")
+			else:
+				self.always_skip.CurrentValue = True
+				self.training_text("Always Skip: On")
+				self.skip_dialog()
+			SaveModSettings(instance)
 	
 	@Hook("GearboxFramework.GearboxDialogComponent.TriggerEvent")
 	def trigger_event(self, this: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
